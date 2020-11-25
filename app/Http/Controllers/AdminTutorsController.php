f9ceac1c4de507d29c55d1428cd10e37ffebbe1a
hafijur <me.hafijur@gmail.com>
2020-11-25 20:46:38 +0600

    tutor account registration from admin panel

--- app/Http/Controllers/AdminTutorsController.php
+++ app/Http/Controllers/AdminTutorsController.php
@@ -1,6 +1,10 @@
 <?php namespace App\Http\Controllers;
 
 use crocodicstudio\crudbooster\controllers\CBController;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Tutor;
+use App\TutorPersonalInformation;
 
 class AdminTutorsController extends CBController {
 
@@ -26,12 +30,45 @@ class AdminTutorsController extends CBController {
 		$this->addText("Available To","available_to")->strLimit(150)->maxLength(255);
 		
 
-	}
+	} 
 	public function getSingle($id){
 		if(!module()->canBrowse()) return cb()->redirect(cb()->getAdminUrl(),cbLang("you_dont_have_privilege_to_this_area"));
 		$tutor=Tutor::findOrFail($id);
 		$page_title="Tutor Profile - ".$tutor->user->name;
 		// dd($tutor);
 		return view('admin.tutor.single',compact('tutor','page_title'));
+	}
+
+	public function getNew(){
+		$page_title="New Tutor";
+		return view('admin.tutor.new',\compact('page_title'));
+	}
+	public function postNew(){
+		$request=request();
+		$request->validate([
+            'name' => 'required|max:100|min:3',
+            'email' => 'required|unique:users|max:255',
+            'phone' => 'required|unique:users|max:13|min:11',
+        ]);
+        // dd($request);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'sms_otp' => rand(99999,999999),
+            'cb_roles_id' => 3,
+            'phone_verified_at' => now(),
+            'email_verified_at' => now(),
+            'password' => Hash::make("Tuition123#"),
+        ]);
+        $tutor=Tutor::create([
+            'user_id'  => $user->id
+        ]);
+        $tutor->save_tutor_id();
+        TutorPersonalInformation::create([
+            'tutor_id'  => $tutor->id
+            // 'tutor_id'  => 1
+		]);
+		return redirect()->back()->with('success','New Tutor Account has been registered successfully');
 	}
 }
